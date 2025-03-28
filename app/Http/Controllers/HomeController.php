a89ec63b98a92898589c510cefbe6f3cc2609880
adminsmc <adminsmc@DLOG-PC20-019.corp.ojk.go.id>
2025-03-28 22:14:30 +0700

    dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+use App\Models\AbsenModel;
+use App\Models\IzinabsenModel;
+use App\Models\PegawaiModel;
 use Illuminate\Http\Request;
+use carbon\Carbon;
 
 class HomeController extends Controller
 {
@@ -23,6 +28,23 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $harini = Carbon::now()->format('Y-m-d');
+//buat per akun kantor pakai Auth
+
+        $rekap = AbsenModel::whereMonth('tgl_absen', carbon::now()->format('m'))
+                ->where('tgl_absen', $harini)
+                ->selectRaw('COUNT(nip) as jmlhadir, SUM(CASE WHEN jam_in > "07:00" THEN 1 ELSE 0 END) as jmltelat')
+                ->first();
+
+
+        $rekapizin = IzinabsenModel::where('tanggal', $harini)
+                    ->selectRaw("
+                        SUM(CASE WHEN jenis_izin = 'i' THEN 1 ELSE 0 END) as izin, 
+                        SUM(CASE WHEN jenis_izin = 's' THEN 1 ELSE 0 END) as sakit
+                    ")
+                    ->where('status_approve', 1)
+                    ->first();
+
+        return view('home', compact('rekap', 'rekapizin'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.side.side')
 
-@section('content')
+{{-- @section('content')
 <div class="container" style="height: 100%;">
     <div class="row justify-content-center" style="height: 100%;">
         <div class="col mw-100">
@@ -59,5 +59,69 @@
             </div>
         </div>
     </div>
+</div>
+@endsection --}}
+
+@section('content')
+<div class="container-xl">
+    <div class="row g-2 align-items-center">
+      <div class="col">
+        <!-- Page pre-title -->
+        <div class="page-pretitle">Dashboard</div>
+        <h2 class="page-title">Data Pegawai</h2>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="row px-1">
+            <div class="col-md-3 mb-2">
+                <div class="card shadow-sm p-2">
+                    <div class="card-body d-flex align-items-center" style="background-color: #e6f4ea; border-radius: 8px;">
+                        <i class="bi bi-fingerprint text-success" style="font-size: 2rem; margin-right: 10px;"></i>
+                        <div>
+                            <h6 class="mb-0">{{$rekap->jmlhadir}}</h6>
+                            <small>Karyawan Hadir</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 mb-2">
+                <div class="card shadow-sm p-2">
+                    <div class="card-body d-flex align-items-center" style="background-color: #e6f0ff; border-radius: 8px;">
+                        <i class="bi bi-file-earmark-text text-primary" style="font-size: 2rem; margin-right: 10px;"></i>
+                        <div>
+                            <h6 class="mb-0">{{$rekapizin->izin}}</h6>
+                            <small>Karyawan Izin</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 mb-2">
+                <div class="card shadow-sm p-2">
+                    <div class="card-body d-flex align-items-center" style="background-color: #fff4e6; border-radius: 8px;">
+                        <i class="bi bi-emoji-frown text-warning" style="font-size: 2rem; margin-right: 10px;"></i>
+                        <div>
+                            <h6 class="mb-0">{{$rekapizin->sakit}}</h6>
+                            <small>Karyawan Sakit</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-3 mb-2">
+                <div class="card shadow-sm p-2">
+                    <div class="card-body d-flex align-items-center" style="background-color: #ffe6e6; border-radius: 8px;">
+                        <i class="bi bi-alarm text-danger" style="font-size: 2rem; margin-right: 10px;"></i>
+                        <div>
+                            <h6 class="mb-0">{{$rekap->jmltelat}}</h6>
+                            <small>Karyawan Terlambat</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
 </div>
 @endsection
