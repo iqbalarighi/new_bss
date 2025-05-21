75b1c2b6b87704412d397d9ce49b9969e2afbebb
iqbalarighi <arighialfarisi@gmail.com>
2025-05-21 07:06:52 +0700

    narrow

--- resources/views/pegawai/excelview.blade.php
+++ resources/views/pegawai/excelview.blade.php
@@ -10,12 +10,15 @@
 
 <head>
   <meta charset="utf-8">
-  <title>Laporan Presensi Karyawan</title>
+  <title>Laporan Presensi Pegawai</title>
 
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/paper-css/0.4.1/paper.css">
     <style>
-   @page { size: A4 landscape }
+   @page { 
+    size: A4 landscape;
+    margin: 0.5cm 0.5cm 0.5cm 0.5cm;
+  }
 
   body {
     font-size: 8px;
